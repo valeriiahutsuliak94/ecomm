e08b242075ae6b5ee7c01d610bfedce70db5f455
Valeria <valeria.gutsuliak@gmail.com>
2020-09-17 23:24:30 -0500

    delite user

--- ecomm/respositories/users.js
+++ ecomm/respositories/users.js
@@ -47,14 +47,18 @@ class UsersRepository {
     const records = await this.getAll();
     return records.find(record => record.id === id);
   }
+
+  async delete(id) {
+    const records = await this.getAll();
+    const filteredRecords = records.filter(record => record.id !== id);
+    await this.writeAll(filteredRecords);
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
 
-  const user = await repo.getOne('1123123123');
-
-  console.log(user);
+  await repo.delete('1d908e76');
 };
 
 test();
