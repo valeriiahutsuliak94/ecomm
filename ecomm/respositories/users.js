80c35e651df09449b6c919498460d8a6bcaae16b
Valeria <valeria.gutsuliak@gmail.com>
2020-09-17 23:21:16 -0500

    delite user

--- ecomm/respositories/users.js
+++ ecomm/respositories/users.js
@@ -42,16 +42,19 @@ class UsersRepository {
   randomId() {
     return crypto.randomBytes(4).toString('hex');
   }
+
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find(record => record.id === id);
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
 
-  await repo.create({ email: 'test@test.com', password: 'password' });
+  const user = await repo.getOne('1123123123');
 
-  const users = await repo.getAll();
-
-  console.log(users);
+  console.log(user);
 };
 
 test();
